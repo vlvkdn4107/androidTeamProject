bda577121101d694a068d877e63c96d337f06feb
kgh <k2hyun2000@hanmail.net>
2022-06-08 16:17:48 +0900

    molontube

--- MelonTubeProject/app/src/main/java/com/example/melontubeproject/SplashActivity.java
+++ MelonTubeProject/app/src/main/java/com/example/melontubeproject/SplashActivity.java
@@ -2,13 +2,43 @@ package com.example.melontubeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.example.melontubeproject.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.silde_side);
+        binding.splashImage.startAnimation(animation);
+        binding.splashTitle.startAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2200);
     }
 }
