65c7c0fc3bb7a7d625e2a1ed4c0e7a5b81cccdd5
jihyeon <pjh4794@gmail.com>
2022-06-09 23:26:04 +0900

    POST 메서드 추가

--- spring_server/src/main/java/com/example/spring_server/controller/ApiController.java
+++ spring_server/src/main/java/com/example/spring_server/controller/ApiController.java
@@ -1,6 +1,8 @@
 package com.example.spring_server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,16 @@ public class ApiController {
 		Data data = new Data();
 		data.setMusicList(Music.sampledata());
 		return data;
+	}
+	
+	@PostMapping("/addmylist")
+	public Music addMyList(@RequestBody Music myMusic) {
+		Music music = new Music();
+		music.setTitle(myMusic.getTitle());
+		music.setSinger(myMusic.getSinger());
+		music.setImageUrl(myMusic.getImageUrl());
+		music.setLyrics(myMusic.getLyrics());
+		return music;
 	}
 
 }
